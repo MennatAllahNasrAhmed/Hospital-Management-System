e743ca77c9c065dabae8e4af37b152f7e3860e2a
MennatAllahNasrAhmed <mennanasrahmed@gmail.com>
2026-08-18 00:09:06 +0300

    Add Person class

---
+++ person.py
@@ -0,0 +1,29 @@
+class Person:
+    """
+    Base class for all people in the hospital.
+
+    Attributes:
+        name (str): The name of the person.
+        age (int): The age of the person.
+    """
+
+    def __init__(self, name, age):
+        """
+        Initializes the Person object.
+
+        Args:
+            name (str): The name of the person.
+            age (int): The age of the person.
+        """
+        self.name = name
+        self.age = age
+
+    def view_info(self):
+        """
+    View basic information about the person.
+
+        Returns:
+            str: The person's name and age.
+        """
+        return f"Name: {self.name}, Age: {self.age}"
+    
